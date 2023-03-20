d291b349cc0db2fa73f10e2be1d0daa32a30c2b5
swknjj <sinwook9255@gmail.com>
2023-03-20 15:15:07 +0900

    interface

---
+++ BoardServiceInt.java
@@ -0,0 +1,17 @@
+package 교수님github.day19_20230323_BoardInterface_01;
+
+import java.util.Map;
+
+public interface BoardServiceInt {
+	public void save();
+	
+	public void findAll();
+	
+	public void findById();
+	
+	public void update();
+	
+	public void delete();
+	
+	public void testData();
+}
